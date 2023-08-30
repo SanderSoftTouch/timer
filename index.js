8982d1d1283b61b158a945b9fb0acd3b39451e7c
Sander <sander.v@softtouch.be>
2023-08-30 16:27:09 +0200

    namiddagprutsingsv1.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var container = document.getElementById("container");
 const play_buttons = document.querySelectorAll('.playstop');
+const save_buttons = document.querySelectorAll('.toevoegen');
 teller_1 = 0;
 teller_2 = 0;
 var timerOn;
@@ -7,16 +8,15 @@ var timerOn;
 for(var i=0; i < play_buttons.length; i++){
     play_buttons[i].addEventListener("click", function(){
         //this.parentElement.id.charAt(this.parentElement.id.length - 1)
-        console.log("werkt", this.parentElement.id, this.innerText)
         toggleButton(this)
     });
-
 }
 
-//const timerOn = setInterval(startTimer(), 1000);
-
-function stopTimer(){
-    clearInterval(timerOn)
+for(var i=0; i < save_buttons.length; i++){
+    save_buttons[i].addEventListener("click", function(){
+        console.log("werkt")
+        slaOp(this)
+    });
 }
 
 function toggleButton(knop){
@@ -24,17 +24,44 @@ function toggleButton(knop){
         knop.innerText = 'Stop'
         knop.style.backgroundColor = "Red"
         var teller = window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`]
-        timerOn = setInterval(function(){
+        timerOn = setInterval(function(){ //start_timer
             teller ++;
-            console.log(teller);
+            //console.log(teller);
+            window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`] = teller;
+            knop.previousElementSibling.innerText = tijdConverter(teller)
         }, 1000);
-        window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`] = teller;
-        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`], teller)
-        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`])
     } else {
         knop.innerText = 'Start'
         knop.style.backgroundColor = "Green"
-        stopTimer()
-        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`], teller)
+        clearInterval(timerOn) //stop_timer
     }
 }
+
+function tijdConverter(nummer){
+    var uur = Math.floor(nummer / 3600);
+    var min = Math.floor(nummer / 60) - uur*60;
+    var sec = nummer%60
+    //console.log(sec, sec < 10, (sec < 10 ? "0" : ""))
+    return `${(uur < 10 ? "0" : "")}${uur}:${(min < 10 ? "0" : "")}${min}:${(sec < 10 ? "0" : "")}${sec}`
+}
+
+function tellerConverter(tijd){
+    const tijdteller = tijd.split(":")
+    var uur = parseInt(tijdteller[0])*3600
+    var min = parseInt(tijdteller[1])*60
+    var sec = parseInt(tijdteller[2])
+    return uur + min + sec
+}
+
+function slaOp(knop){
+    (knop.previousElementSibling.innerText == "Stop" ? toggleButton(knop.previousElementSibling) : null)
+    var resultaat = window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`]
+    window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`] = 0
+    knop.previousElementSibling.previousElementSibling.innerText = '00:00:00'
+    knop.nextElementSibling.childNodes[1].innerText = tijdConverter(tellerConverter(knop.nextElementSibling.childNodes[1].innerText) + resultaat);
+    console.log(resultaat, dagtotaal, knop.nextElementSibling.childNodes[1], tellerConverter("01:01:01"), knop.previousElementSibling)
+}
+
+
+
+//console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`], teller)
