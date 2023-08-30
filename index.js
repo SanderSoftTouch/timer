3ecef63a277a88e6035bf3056300ada34f7df912
Sander <sander.v@softtouch.be>
2023-08-30 14:32:29 +0200

    namiddagprutsingsv1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("test")
+var container = document.getElementById("container");
+const play_buttons = document.querySelectorAll('.playstop');
+teller_1 = 0;
+teller_2 = 0;
+var timerOn;
+
+for(var i=0; i < play_buttons.length; i++){
+    play_buttons[i].addEventListener("click", function(){
+        //this.parentElement.id.charAt(this.parentElement.id.length - 1)
+        console.log("werkt", this.parentElement.id, this.innerText)
+        toggleButton(this)
+    });
+
+}
+
+//const timerOn = setInterval(startTimer(), 1000);
+
+function stopTimer(){
+    clearInterval(timerOn)
+}
+
+function toggleButton(knop){
+    if(knop.innerText == 'Start'){
+        knop.innerText = 'Stop'
+        knop.style.backgroundColor = "Red"
+        var teller = window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`]
+        timerOn = setInterval(function(){
+            teller ++;
+            console.log(teller);
+        }, 1000);
+        window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`] = teller;
+        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`], teller)
+        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`])
+    } else {
+        knop.innerText = 'Start'
+        knop.style.backgroundColor = "Green"
+        stopTimer()
+        console.log(window[`teller_${knop.parentElement.id.charAt(knop.parentElement.id.length - 1)}`], teller)
+    }
+}
